5e8f83bf725d4a0d3cd35bc3ad3bf4efadc37e7c
HunterJiang <15858149091@163.com>
2023-03-30 17:38:01 +0800

    style: 自定义样式

--- docs/components/Card/index.vue
+++ docs/components/Card/index.vue
@@ -9,33 +9,40 @@ defineProps<{
 
 <template>
   <div class="card">
-    <div class="title">
-      <h2>
-        <a :href="link">
-          {{ title }}
-        </a>
-      </h2>
-    </div>
+    <h2 class="title">
+      <a :href="link">
+        {{ title }}
+      </a>
+    </h2>
     <div class="description">
       {{ description }}
     </div>
+    <slot name="footer"></slot>
   </div>
 </template>
 
-<style scoped>
+<style lang="less" scoped>
 .card {
   display: flex;
   flex-direction: column;
   padding: 24px;
   height: 100%;
-  color: rgba(60, 60, 67);
-  background-color: rgb(246, 246, 247);
+  display: block;
+  border: 1px solid var(--vp-c-bg-soft);
+  border-radius: 12px;
+  height: 100%;
+  background-color: var(--vp-c-bg-soft);
+  transition: border-color 0.25s, background-color 0.25s;
   font-size: 16px;
   font-weight: 400;
   line-height: 24px;
 }
-.card .title:hover {
-  color: #747bff;
+.card .title {
+  font-size: 16px;
+  font-weight: 600;
+  &:hover {
+    color: var(--vp-button-brand-bg);
+  }
 }
 .card .description {
   flex-grow: 1;
@@ -43,6 +50,6 @@ defineProps<{
   line-height: 24px;
   font-size: 14px;
   font-weight: 500;
-  color: rgba(60, 60, 67, 0.75);
+  color: var(--vp-c-text-2);
 }
 </style>
